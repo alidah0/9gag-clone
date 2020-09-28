ef9a3d6267c9e5fd771bf1e0ef71448f8cbef61e
alidah0 <ali.dahdouh@gmail.com>
2020-09-28 16:30:56 +0300

    added exchange graphcache to customize cache relates #13

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,14 +1,76 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-import { createClient, Provider } from "urql";
+import {
+  cacheExchange,
+  Cache,
+  QueryInput,
+  query,
+} from "@urql/exchange-graphcache";
+import { createClient, Provider, dedupExchange, fetchExchange } from "urql";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+import theme from "../theme";
+
+function betterUpdateQuery<Result, Query>(
+  cach: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cach.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-
-import theme from "../theme";
 
 function MyApp({ Component, pageProps }: any) {
   return (
