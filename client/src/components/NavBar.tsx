84df06571787977ef106bb7a1b2ab2cfbfd78c68
alidah0 <ali.dahdouh@gmail.com>
2020-10-10 12:27:30 +0300

    created post page and updated navbar and index styling relates #53

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, Flex, Link } from "@chakra-ui/core";
+import { Box, Button, Flex, Heading, Link } from "@chakra-ui/core";
 import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
 import { isServer } from "../utils/isServer";
+import { link } from "fs";
 
-const Navbar: React.FC<{}> = ({}) => {
+const Navbar: React.FC<{}> = ({ }) => {
   const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery({
     pause: isServer(),
@@ -25,7 +26,15 @@ const Navbar: React.FC<{}> = ({}) => {
     );
   } else {
     body = (
-      <Flex>
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            <Link mr="2">
+              Create Post
+          </Link>
+          </Button>
+
+        </NextLink>
         <Box mr="3">{data.me.username}</Box>
         <Button
           onClick={() => logout()}
@@ -40,9 +49,15 @@ const Navbar: React.FC<{}> = ({}) => {
   }
 
   return (
-    <Flex zIndex={1} position="sticky" top="0" p={5} bg="Teal">
-      <h1>Discuss Mania</h1>
-      <Box ml="auto">{body}</Box>
+    <Flex zIndex={1} position="sticky" top="0" p={5} alignItems="center" bg="Teal">
+      <Flex flex={1} m="auto" maxW={800} align="center">
+        <NextLink href="/">
+          <Link>
+            <Heading>Discuss Mania</Heading>
+          </Link>
+        </NextLink>
+        <Box ml="auto">{body}</Box>
+      </Flex>
     </Flex>
   );
 };
---
+++ client/src/pages/post/[id].tsx
@@ -0,0 +1,45 @@
+import { Box, Heading } from '@chakra-ui/core';
+import { withUrqlClient } from 'next-urql';
+import { useRouter } from 'next/router';
+import React from 'react'
+import Layout from '../../components/Layout';
+import { usePostQuery } from '../../generated/graphql';
+import { createUrqlClient } from '../../utils/createUrqlClient';
+
+const Post = ({ }) => {
+  const router = useRouter()
+  const intId = typeof router.query.id === "string" ? parseInt(router.query.id) : -1
+  const [{ data, fetching, error }] = usePostQuery({
+    variables: {
+      id: intId
+    }
+  })
+
+  if (fetching) {
+    return (
+      <Layout><Box>Loading...</Box></Layout>
+    )
+  }
+
+  if (error) {
+    return (
+      <Layout><Box>{error}</Box></Layout>
+    )
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout><Box>could not find a post</Box></Layout>
+    )
+  }
+
+  return (<Layout>
+    <Heading mb={4}>{data.post.title}</Heading>
+    {data.post.text}</Layout>)
+}
+
+
+
+
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
