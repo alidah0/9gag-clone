b03f3b45a11bdb2b6c748e7580c19a3cefe786b3
alidah0 <ali.dahdouh@gmail.com>
2020-10-11 16:16:54 +0300

    created a front update post form and button in the post card relates #57

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import { useDeletePostMutation, usePostsQuery } from "../generated/graphql";
+import {
+  useDeletePostMutation,
+  useMeQuery,
+  usePostsQuery,
+} from "../generated/graphql";
 import {
   Box,
   Heading,
@@ -16,13 +20,13 @@ import {
 import Layout from "../components/Layout";
 import NextLink from "next/link";
 import { UpdootSection } from "../components/UpdootSection";
-import Id from "./post/[id]";
 
 const Index = () => {
   const [variables, setVariables] = useState({
     limit: 15,
     cursor: null as null | string,
   });
+  const [{ data: meData }] = useMeQuery();
   const [{ data, fetching }] = usePostsQuery({
     variables,
   });
@@ -38,37 +42,51 @@ const Index = () => {
       ) : !fetching && !data ? (
         <div>Something went wrong!, Query failed!!</div>
       ) : (
-            <Stack spacing={8}>
-              {data?.posts.posts.map((p) =>
-                !p ? null : (
-                  <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
-                    <UpdootSection post={p} />
-                    <Box flex={1}>
-                      <NextLink href="/post/[id]" as={`/post/${p.id}`}>
-                        <Link>
-                          <Heading fontSize="xl">{p.title}</Heading>
-                        </Link>
-                      </NextLink>
-                      <Text>posted by {p.creator.username}</Text>
-                      <Flex align="center">
-                        <Text flex={1} mt={4}>
-                          {p.textSnippet}
-                        </Text>
+        <Stack spacing={8}>
+          {data?.posts.posts.map((p) =>
+            !p ? null : (
+              <Flex key={p.id} p={5} shadow="md" borderWidth="1px">
+                <UpdootSection post={p} />
+                <Box flex={1}>
+                  <NextLink href="/post/[id]" as={`/post/${p.id}`}>
+                    <Link>
+                      <Heading fontSize="xl">{p.title}</Heading>
+                    </Link>
+                  </NextLink>
+                  <Text>posted by {p.creator.username}</Text>
+                  <Flex align="center">
+                    <Text flex={1} mt={4}>
+                      {p.textSnippet}
+                    </Text>
+                    {meData?.me?.id !== p.creator.id ? null : (
+                      <Box ml="auto">
+                        <NextLink
+                          href="/post/edit/[id]"
+                          as={`/post/edit/${p.id}`}
+                        >
+                          <IconButton
+                            as={Link}
+                            icon="edit"
+                            mr={4}
+                            aria-label="Edit Post"
+                          />
+                        </NextLink>
                         <IconButton
-                          variantColor="red"
                           icon="delete"
                           aria-label="Delete Post"
                           onClick={() => {
                             deletePost({ id: p.id });
                           }}
                         />
-                      </Flex>
-                    </Box>
+                      </Box>
+                    )}
                   </Flex>
-                )
-              )}
-            </Stack>
+                </Box>
+              </Flex>
+            )
           )}
+        </Stack>
+      )}
       <Flex>
         {data && data.posts.hasMore ? (
           <Button
---
+++ client/src/pages/post/edit/[id].tsx
@@ -0,0 +1,87 @@
+import { Box, Button, Heading } from "@chakra-ui/core";
+import { Formik, Form } from "formik";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import InputField from "../../../components/InputField";
+import Layout from "../../../components/Layout";
+import {
+  usePostQuery,
+  useUpdatePostMutation,
+} from "../../../generated/graphql";
+import { createUrqlClient } from "../../../utils/createUrqlClient";
+import { getIntId } from "../../../utils/getIntId";
+
+const EditPost: React.FC<{}> = ({}) => {
+  const router = useRouter();
+  const intId = getIntId();
+  const [{ data, fetching, error }] = usePostQuery({
+    variables: {
+      id: intId,
+    },
+  });
+
+  const [_, updatePost] = useUpdatePostMutation();
+  if (fetching) {
+    return (
+      <Layout>
+        <Box>Loading...</Box>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <Box>{error}</Box>
+      </Layout>
+    );
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <Box>could not find a post</Box>
+      </Layout>
+    );
+  }
+  return (
+    <Layout variant="small">
+      <Formik
+        initialValues={{ title: data.post.title, text: data.post.text }}
+        onSubmit={async (values) => {
+          console.log({ values });
+          const { error } = await updatePost({ id: intId, ...values });
+          if (!error) {
+            router.back();
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="Title..." label="Title" />
+            <Box mt={4}>
+              <InputField
+                textarea
+                name="text"
+                placeholder="Text..."
+                label="Body"
+              />
+            </Box>
+            <Box mt={4}>
+              <Button
+                type="submit"
+                variantColor="teal"
+                isLoading={isSubmitting}
+              >
+                Create Post
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient)(EditPost);
