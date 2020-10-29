5b135ff232a9648cc2613aa9a67f9897fec83d9f
alidah0 <ali.dahdouh@gmail.com>
2020-10-29 10:48:20 +0200

    added redis in the server and the env

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -1,9 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     DATABASE_URL: string;
-    REDIS_URL: string;
     PORT: string;
     SESSION_SECRET: string;
     CORS_ORIGIN: string;
+    REDIS_URL: string;
   }
 }
