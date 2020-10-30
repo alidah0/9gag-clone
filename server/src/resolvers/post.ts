ff035f52c934192e3a68907116130520bb440a9c
alidah0 <ali.dahdouh@gmail.com>
2020-10-30 18:26:27 +0200

    added new field to entites and updated the post resolvers relates #77

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -25,6 +25,8 @@ class PostInput {
   title: string;
   @Field()
   text: string;
+  @Field()
+  memePic: string;
 }
 
 @ObjectType()
@@ -165,6 +167,7 @@ export class PostResolver {
     @Arg("input") input: PostInput,
     @Ctx() { req }: MyContext
   ): Promise<Post> {
+    console.log(input);
     return Post.create({
       ...input,
       creatorId: req.session.userId,
@@ -177,12 +180,13 @@ export class PostResolver {
     @Arg("id", () => Int) id: number,
     @Arg("title") title: string,
     @Arg("text") text: string,
+    @Arg("memePic") memePic: string,
     @Ctx() { req }: MyContext
   ): Promise<Post | null> {
     const result = await getConnection()
       .createQueryBuilder()
       .update(Post)
-      .set({ title, text })
+      .set({ title, text, memePic })
       .where('id = :id and "creatorId" = :creatorId', {
         id,
         creatorId: req.session.userId,
